5952740801de5f901d9f7ce6be440582c1d653c8
Geno [格诺] <genokolar@gmail.com>
2021-09-03 08:33:00 +0800

    Fix compile error

--- nRF5_SDK_17.0.2_d674dde/examples/ble_peripheral/ble_app_uart_ESB/ESB_Timeslot/esb_timeslot.h
+++ nRF5_SDK_17.0.2_d674dde/examples/ble_peripheral/ble_app_uart_ESB/ESB_Timeslot/esb_timeslot.h
@@ -39,7 +39,7 @@ void configure_next_event_normal(void);
 
 /**@brief Timeslot signal handler
  */
-void nrf_evt_signal_handler(uint32_t evt_id);
+void nrf_evt_signal_handler(uint32_t evt_id, void* p_context);
 
 
 /**@brief Timeslot event handler
